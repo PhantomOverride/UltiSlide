fbdd4b0d1f992590495183be2ab1a683f283189c
Olof Haglund <olof@olofhaglund.name>
2016-07-24 22:03:53 +0200

    Added a new file to replace the index file

---
+++ ultislide.php
@@ -0,0 +1,20 @@
+<canvas id="MKslide">
+</canvas>
+
+<iframe id="youtube" width="560" height="315" src="" frameborder="0" allowfullscreen></iframe>
+<iframe id="website" width="500" height="500" src="" frameborder="0" ></iframe>
+
+<div><!-- blinker -->
+    <div class="blinker" id="topLeft">
+
+    </div>
+    <div class="blinker" id="topRight">
+
+    </div>
+    <div class="blinker" id="bottomLeft">
+
+    </div>
+    <div class="blinker" id="bottomRight">
+
+    </div>
+</div>
