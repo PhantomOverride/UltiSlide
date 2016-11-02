32e52288f9bf1c899cd94d2efd26a6fde5fecc36
Oscar <contact@oscarhjelm.com>
2016-11-02 21:08:40 +0100

    Fixed slideshow frontend.

--- ultislide.php
+++ ultislide.php
@@ -1,35 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <body>
+    <script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+    <script>
+        var UltiSlide = function(){};
+        UltiSlide.settings = {
+            audioFile : "MoveMove.wav",
+            blinkTime : 1000,
+            size : {
+                width: $(document).width(),
+                height: $(document).height(),
+            },
+        };
+    </script>
+    <script src="slider.js"></script>
+    <canvas id="ultiSlide"></canvas>
 
-<link rel="stylesheet" type="text/css" href="style.css"/>
-<script>
-var UltiSlide = function(){};
-UltiSlide.settings = {
-    audioFile : "MoveMove.wav",
-    blinkTime : 1000,
-    size : {
-        width: $(document).width(),
-        height: $(document).height(),
-    },
-};
-</script>
-<script src="slider.js">
-</script>
-<canvas id="ultiSlide">
-</canvas>
+    <iframe id="youtube" width="560" height="315" src="" frameborder="0" allowfullscreen></iframe>
+    <iframe id="website" width="500" height="500" src="" frameborder="0" ></iframe>
 
-<iframe id="youtube" width="560" height="315" src="" frameborder="0" allowfullscreen></iframe>
-<iframe id="website" width="500" height="500" src="" frameborder="0" ></iframe>
+        <div><!-- blinker -->
+            <div class="blinker" id="topLeft">
 
-<div><!-- blinker -->
-    <div class="blinker" id="topLeft">
+            </div>
+            <div class="blinker" id="topRight">
 
-    </div>
-    <div class="blinker" id="topRight">
+            </div>
+            <div class="blinker" id="bottomLeft">
 
-    </div>
-    <div class="blinker" id="bottomLeft">
+            </div>
+            <div class="blinker" id="bottomRight">
 
-    </div>
-    <div class="blinker" id="bottomRight">
-
-    </div>
-</div>
+            </div>
+        </div>
+    </body>
+</html>
