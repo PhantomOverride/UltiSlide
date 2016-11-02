2671955192f2bbce8b8ee8eac238f4df26bfce47
Oscar <contact@oscarhjelm.com>
2016-11-02 22:01:03 +0100

    Typo in config

--- cms/php/config.example.php
+++ cms/php/config.example.php
@@ -3,7 +3,7 @@
     define("API_KEY", "SET_YOUR_GOOGLE_API_TOKEN_HERE"); // Set you google api avalible at http://console.developers.google.com . You also have to activate the "YouTube Data API v3".
     define("URL_TO_SLIDE", "example.com/"); //url to the base dircetory for the slide
     define("IMAGES", URL_TO_SLIDE . "images/"); //External path to image directory
-    define("IMAGE_DIRECTORY", "../../images/  "); //Internal (relative) path to the image directory
+    define("IMAGE_DIRECTORY", "../../images/"); //Internal (relative) path to the image directory
 
     class DataBase{
             private static $DSN 	= "mysql:host=localhost;dbname=DATABASE_NAME";
